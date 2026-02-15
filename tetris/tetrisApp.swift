de9830bbf807b15e8257f4226eaea24db452f920
Linda <lindhcn@gmail.com>
2026-02-15 11:07:06 -0500

    Initial Commit

---
+++ tetris/tetrisApp.swift
@@ -0,0 +1,32 @@
+//
+//  tetrisApp.swift
+//  tetris
+//
+//  Created by linda han on 2026-02-15.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct tetrisApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ tetrisTests/tetrisTests.swift
@@ -0,0 +1,17 @@
+//
+//  tetrisTests.swift
+//  tetrisTests
+//
+//  Created by linda han on 2026-02-15.
+//
+
+import Testing
+@testable import tetris
+
+struct tetrisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
